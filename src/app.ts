2bc06da70b89f64f1ef0c357ee2e6f6ac24c4e1e
pemiinem <pedro.m.rodiguero@gmail.com>
2022-07-28 09:39:46 -0300

    fix bug cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
-require('dotenv').config()
-
+import cors from 'cors';
 import express from 'express';
 import router from './routes/router';
+
+require('dotenv').config();
 
 const app = express();
 app.disable('x-powered-by');
@@ -10,5 +11,6 @@ app.disable('x-powered-by');
 app.use(express.json());
 
 app.use(router);
+app.use(cors());
 
 export default app;
